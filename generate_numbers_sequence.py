1bd1efd9974d2725299dce81b34292952f2e81c4
Arikanev <arikanevsky@Aris-MacBook-Air.local>
2018-02-13 18:53:01 -0500

    added helper methods for extensibility,readability

--- generate_numbers_sequence.py
+++ generate_numbers_sequence.py
@@ -41,38 +41,64 @@ def generate_numbers_sequence(digits, spacing_range, image_width):
     Space is saved by recomputing idx_mask for each sequence digit.
 
     """
-    """
-    Initialize digit_sequence with:
+    data = load_pickle('mnist.pkl')
 
-        elements: 0 (black)
-        height: 28
-        width: int randomly selected from spacing_range.
+    # Pool train, test, val images.
 
-    np.random.random_integers is [low, high] (inclusive),
-     as opposed to np.random.randint's [low, high).
-    """
-    spacing = np.random.random_integers(spacing_range[0], spacing_range[1])
-    digit_sequence = np.zeros((28, spacing))
+    X, y = pool_data(data)
 
+    # Initalize digit_sequence with (height, width) and fill with zeros.
+
+    digit_sequence = np.zeros((28, sample_spacing(spacing_range)))
+
+    # Iterate through digits.
+
+    for digit in digits:
+
+        # Sample an mnist digit.
+
+        digit = sample_image(X, y, digit)
+
+        # Generate a spacing.
+
+        spacing = np.zeros((28, sample_spacing(spacing_range)))
+
+        # Append digit to current sequence, and spacing to image.
+
+        digit_sequence = np.hstack((digit_sequence, digit, spacing))
+
+    # Alter width
+
+    digit_sequence = alter_width(digit_sequence, image_width)
+
+    # Return digit_sequence.
+
+    return digit_sequence
+
+
+def load_pickle(pklfile):
     """
+    Use as helper method.
+
     Here we load in mnist from a .pkl file. Using pickle to store/load/
-    transport data is a memory efficient method. An alternative but
-    considerably slower method of loading mnist would be iterating
+    transport data is a memory efficient method.
+
+    An alternative but slower method of loading mnist would be iterating
     through a directory of mnist images.
 
     Due to a compatibility problem with numpy in 2.7 vs 3.x, we must specify
     an encoding for pkl.load() to read the mnist file properly.
     """
+    return pkl.load(open(pklfile, 'rb'), encoding='latin1')
 
-    data = pkl.load(open('mnist.pkl', 'rb'), encoding='latin1')
 
+def pool_data(data):
     """
+    Use as helper method.
+
     Here we stack train, test, val examples and labels to 'pool'
     all images together.
     """
-
-    # Vertically stack images.
-
     X = np.vstack((data[0][0], data[1][0], data[2][0]))
 
     # Reshape images to fit a (height, width) construct.
@@ -83,56 +109,91 @@ def generate_numbers_sequence(digits, spacing_range, image_width):
 
     y = np.hstack((data[0][1], data[1][1], data[2][1]))
 
-    for digit in digits:
+    return X, y
 
-        """
-        idx_mask: numpy array of boolean values, specifying if
-        mnist image at idx_mask matches the current digit in the sequence.
-        """
-        # Get idx_mask for current digit.
 
-        idx_mask = (y == np.full((70000), digit))
+def sample_spacing(spacing_range):
+    """
+    Use as helper method.
 
-        # Extract relevant images.
+    Here we sample a random image spacing from a range.
 
-        pool = X[idx_mask]
+    np.random.random_integers is [low, high] (inclusive),
+    as opposed to np.random.randint's [low, high).
+    """
+    return np.random.random_integers(spacing_range[0], spacing_range[1])
 
-        """
-        Trade-off on random selection
-        -----------------------------
-        Rather than another dependancy, we use numpy and a more complex line.
-        """
 
-        # Sample random mnist image from pool.
+def sample_image(X, y, digit):
+    """
+    Use as helper method.
 
-        image = pool[np.random.randint(len(pool))]
+    idx_mask: numpy array of boolean values, specifying if
+    mnist image at idx_mask matches the current digit in the sequence.
+    """
+    idx_mask = (y == np.full((70000), digit))
 
-        # Generate a spacing filled with 0 (black values)
-        spacing = np.random.random_integers(spacing_range[0], spacing_range[1])
-        spacing = np.zeros((28, spacing))
+    # Extract relevant images.
 
-        # Append image to digit_sequence, and 0 (black) values to image.
+    pool = X[idx_mask]
 
-        digit_sequence = np.hstack((digit_sequence, image, spacing))
+    """
+    Trade-off on random selection
+    -----------------------------
+    Rather than another dependancy, we use numpy and a more complex line.
+    """
 
+    # Sample random mnist image from pool.
+
+    return pool[np.random.randint(len(pool))]
+
+
+def numpy2im(digit_sequence):
+    """
+    Use as helper method.
+
+    Converts a numpy array to a PIL Image object.
+    """
+    return Image.fromarray(np.uint8(digit_sequence * 255))
+
+
+def alter_width(digit_sequence, image_width):
+    """
+    Use as helper method.
+
+    Alters the width of a numpy array.
+    Uses interpolation method from PIL Image.
+    """
     # Convert numpy array to PIL Image object.
 
-    image = Image.fromarray(np.uint8(digit_sequence * 255))
+    digit_sequence = numpy2im(digit_sequence)
 
     # Resize generated image width by image_width pixels.
 
-    image = image.resize((image_width, 28), Image.ANTIALIAS)
+    digit_sequence = digit_sequence.resize((image_width, 28), Image.ANTIALIAS)
 
     # Convert PIL Image object to numpy array.
 
-    digit_sequence = np.array(image).astype('float32') / 255
-
-    # Return digit_sequence.
+    digit_sequence = np.array(digit_sequence).astype('float32') / 255
 
     return digit_sequence
 
 
-if __name__ == '__main__':
+def save_sequence(digit_sequence):
+    """
+    Use as helper method.
+
+    Saves a PIL Image object to the current directory as unique file.
+    """
+    # Count number of generated sequence files.
+
+    num_sequence_files = len(glob.glob1(os.getcwd(), 'sequence*'))
+
+    digit_sequence.save('sequence{}.png'.format(num_sequence_files))
+    print("Saved sequence{}.png".format(num_sequence_files))
+
+
+def parse_args():
     """
     Argparse is used here for command-line compatibility/script purposes.
 
@@ -142,7 +203,6 @@ if __name__ == '__main__':
     An alternative method for reading command-line inputs would be
     sys.argv[idx].
     """
-
     parser = argparse.ArgumentParser()
 
     parser._action_groups.pop()
@@ -179,83 +239,76 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    return args
+
+
+def augment_sequence(greyscale_sequence):
+    """
+    Use as helper method.
+
+    Here we have the possibility to augment the sequence in
+    the form of mnistm images.
+
+    A full mnistm set can be found at akanev.com/datasets.
+    """
+    background = load_pickle('imagenet.pkl')
+
+    # Get a random imagenet background from array.
+
+    background = background[np.random.randint(len(background))]
+
+    Image.fromarray(background).save('before_alter_width.png')
+
+    # Alter the width.
+
+    background = np.uint8(alter_width(background, args.image_width) * 255)
+
+    # Convert greyscale sequence to RGB for compatibility.
+
+    digit_sequence = np.empty((28, args.image_width, 3), dtype=np.uint8)
+    digit_sequence[:, :, 0] = np.uint8(greyscale_sequence * 255)
+    digit_sequence[:, :, 1] = digit_sequence[:, :, 0]
+    digit_sequence[:, :, 2] = digit_sequence[:, :, 1]
+
+    # Set sequence values over & under a threshold to imagenet values.
+
+    digit_sequence[digit_sequence < 127] = background[digit_sequence < 127]
+    digit_sequence[digit_sequence > 126] = 255 - background[digit_sequence >
+                                                            126]
+
+    # Convert numpy array to PIL Image object and return.
+
+    return Image.fromarray(digit_sequence)
+
+
+if __name__ == '__main__':
+
+    # Parse command line arguments.
+
+    args = parse_args()
+
     # Create a tuple from min and max spacing range.
 
     spacing_range = (args.spacing_range[0], args.spacing_range[1])
 
     # Generate an image sequence.
 
-    sequence_array = generate_numbers_sequence(digits=args.digits,
+    digit_sequence = generate_numbers_sequence(digits=args.digits,
                                                spacing_range=spacing_range,
                                                image_width=args.image_width)
 
-    # Rescale and convert numpy array to PIL Image object.
+    # Save generated sequence to file.
 
-    sequence = Image.fromarray(np.uint8(sequence_array * 255))
+    save_sequence(numpy2im(digit_sequence))
 
-    # Count number of generated sequence files.
-
-    num_sequence_files = len(glob.glob1(os.getcwd(), 'sequence*'))
-
-    # Save generated sequence as unique file.
-
-    sequence.save('sequence{}.png'.format(num_sequence_files))
-    print("Saved sequence{}.png".format(num_sequence_files))
-
-    """
-    Here we have the possibility to augment the sequence in
-    the form of mnistm images.
-
-    A full mnistm set can be found at akanev.com/datasets.
-    """
-
-    # Use if 'mnistm' is specified in the argparse 'augmentation'.
+    # Use if 'mnistm' is specified in the arg 'augmentation'.
 
     if (args.augmentation == 'mnistm'):
 
-        # Open the imagenet pickle file.
+        # Augment sequence
 
-        with open('imagenet.pkl', 'rb') as infile:
+        augmented_sequence = augment_sequence(digit_sequence)
 
-            # Read pickle file.
+        # Save generated sequence to file.
 
-            imgnet_array = pkl.load(infile)
-
-            # Get a random imgnet_img from array.
-
-            imgnet_img = imgnet_array[np.random.randint(len(imgnet_array))]
-
-            # Convert imgnet_img to PIL Image object.
-
-            imgnet_img = Image.fromarray(imgnet_img)
-
-            # Resize PIL Image object to equal sequence width and height.
-
-            imgnet_img = imgnet_img.resize((args.image_width, 28),
-                                           Image.ANTIALIAS)
-
-            # Convert PIL Image object back to numpy array.
-
-            imgnet_img = np.array(imgnet_img)
-
-            # Convert greyscale sequence to RGB for compatibility.
-
-            rgb_sequence = np.empty((28, args.image_width, 3), dtype=np.uint8)
-            rgb_sequence[:, :, 0] = np.uint8(sequence_array * 255)
-            rgb_sequence[:, :, 1] = rgb_sequence[:, :, 0]
-            rgb_sequence[:, :, 2] = rgb_sequence[:, :, 1]
-
-            # Set sequence values over & under a threshold to imagenet values.
-
-            rgb_sequence[rgb_sequence < 127] = imgnet_img[rgb_sequence < 127]
-            rgb_sequence[rgb_sequence > 126] = 255 - imgnet_img[rgb_sequence >
-                                                                126]
-
-            # Convert numpy array to PIL Image object
-
-            rgb_sequence = Image.fromarray(rgb_sequence)
-
-            # Save generated sequence as unique file.
-
-            rgb_sequence.save('aug_sequence{}.png'.format(num_sequence_files))
-            print("Saved aug_sequence{}.png".format(num_sequence_files))
+        save_sequence(augmented_sequence)
